00b00a4b691fbc1c90b7c3132649f71fa6d89431
Charles Nebo <charles.nebo@hotmail.com>
2024-01-24 18:14:59 +0200

    add Operators and Control Flow, Swift Basics, contents.xc, and playground.xc

--- PushingPlaygroundsToGithub.playground/Pages/Operators and Control Flow .xcplaygroundpage/Contents.swift
+++ PushingPlaygroundsToGithub.playground/Pages/Operators and Control Flow .xcplaygroundpage/Contents.swift
@@ -1,46 +1,129 @@
-// Operators and Control Flow
+import Foundation
+
+//Swift Operators
+
+let additionResult = 10 + 5         // addition
+let subtractionResult = 20 - 8      // substraction
+let multiplicationResult = 6 * 4    // multiplication
+let divisionResult = 24 / 3         // division
+let moduluResult = 15 % 7           // modulus
 
 
+//Comparison Operators
+let isEqual = (10 == 5)             // ==
+let isNotEqual = (20 != 8)          // !=
+let isGreater = (6 > 4)             // >
+let isLess = (24 < 3)               // <
+let isGreaterOrEqual = (15 >= 7)    // >=
 
-//Arithmetic Operators: such as +, - , *, /, % (modulus)
-
-//Comparison Operators: such as ==, !=, <, >, <=, >=
 
 //Logical Operators: such as &&, ||, !
-
-//Assignment Operators:
-
-/**
- == (assign),
- += (add and assign),
- -= (substract and assign),
- *= (muliplication and assign),
- /= divide and assign,
- %= (modulus and assign).
- 
- */
+let andResult = true && false
+let orResult = true || false
+let notResult = !true
+print(notResult)
 
 
+//Assignment Operators
+var x = 5
+x += 3     // x is now 8
+x -= 2     // x is now 6
+x *= 4     // x is now 24
+x /= 3     // x is now 8
 
-//Control flow structures such as:
-/**
- if statements
-   //else
-   //else if
-   //Switch
- */
 
-//Looping Statements
-  /**
-   for loop
-   while loop
-   do-while loop
-   */
+//Nil Coalescing Operator
+let userInput: String? = nil
+let userName = userInput ?? "Guest"
 
-//Branching
- /**
-  break
-  continue
-  return
-  */
+
+//Swift Control Flow
+
+let temperature = 25
+if temperature > 30{
+    print("It is a hot today")
+}
+else{
+    print("It's not too hot")
+}
+
+//Switch Statements
+let dayOfWeek = "Wednesday"
+switch dayOfWeek {
+case "Monday", "Wednesday", "Friday":
+    print("It's a workday.")
+default:
+    print("It's a weekend. ")
+}
+
+// For-in Loop
+let numbers = [1, 2, 3, 4, 5]
+for number in numbers {
+    print("current number is \(number)")
+}
+
+//While Loop
+var counter = 0
+while counter < 5{
+    print("Counter is \(counter)")
+       counter += 1
+}
+     
+
+//Guard Statement
+func processInput(_ input: String?){
+    guard let unwrappedInput = input else {
+        print("input is nil. ")
+        return
+    }
+    print("Processing input: \(unwrappedInput)")
+}
+
+//If Statements including Else and Else if
+let timeOfDay = "morning"
+if timeOfDay == "morning"{
+    print("Good morning!")
+}else if timeOfDay == "afternoon!"{
+    print("Good afternoo!")
     
+}else{
+    print("Good evening!")
+}
+    
+//Do-While Loop
+var repeatCount = 3
+repeat{
+    print("this will be repeated  \(repeatCount) times.")
+    repeatCount -= 1
+}while repeatCount > 0
+
+//Branching Statements (Break, Continue, Return)
+
+//Break
+for i in 1...10 {
+    if i == 5 {
+        break
+    }
+    print(i)
+}
+
+//Continue in a Loop
+for i in 1...10 {
+    if i % 2 == 0 {
+        continue
+    }
+    print(i)
+}
+
+//Return in a function
+
+func multiplyByTwo(_ number: Int) -> Int {
+    return number * 2
+}
+
+let result = multiplyByTwo(5)
+print(result)
+
+
+
+
--- PushingPlaygroundsToGithub.playground/Pages/Swift Basics.xcplaygroundpage/Contents.swift
+++ PushingPlaygroundsToGithub.playground/Pages/Swift Basics.xcplaygroundpage/Contents.swift
@@ -1,23 +1,8 @@
 import Foundation
 
-/**
  
-Swift Variable, Constants, and Basic Data Types
+//Swift Variable, Constants, and Basic Data Types
  
- 
-Variable
-
-Variable is a storage location identified by memory address and associated with a symbolic name(identifier). The name contains some known or unknown value. Variables are used in programming to store and manipulate data during the execution of a programme.
- 
- A Swift variable is a named storage that can contain values of a certain type.
- 
- "var" is the keyword to declar the variable.
- "name" is the variable identifier
- "colon" is used to specify the type of variable
- "=" is used to assign value to the variable
- Swift is a strongly typed language, meaning that each varible must have a specific type , and once a variable is assigned a type, its type cannot be changed 
- */
-
 
 
 //Here are five example of Swift variables:
@@ -37,42 +22,27 @@ str += "!"  //OR
 str = str + "!"
 
 
- /**
- Constants. the key concept is the same with variable above. However, the key distinction from variable is that once a value is assigned to a constant, it cannot be changed throughtout the execution of the program. Constants are used to represent values that remain unchanged during the course of the program. */
- 
- //Constants uses "let" keyword
+//Here a 5 examples of Swift Constants
 
-let nmberOFDaysInAweek: Int = 7 // Integer Constant
+// Integer Constant
+let nmberOFDaysInAweek: Int = 7
 
-let appname: String = "MyApp" //String Constant
 
-let piValue: Double = 3.14159 // Floating - Point Constant
+//String Constant
+let appname: String = "MyApp"
 
-let isSwiftAwesone: Bool = false // Boolean Constant
 
+//Floating - Point Constant
+let piValue: Double = 3.14159
+
+// Boolean Constant
+let isSwiftAwesone: Bool = false
+
+
+//Array Constant
 let primeNumbers: [Int] = [2, 3, 5, 7, 11]
 
-
-
-/**
-
-Differences between Swift Variables and Constants:
-
-Mutability: Variables (var) can be changed after initial assignment.
-Constants (let) cannot be changed after initial assignment.
-Declaration:
-
-Variables are declared using the var keyword.
-Constants are declared using the let keyword.
-
-
-Use Case: Use variables when the value might change during the execution of the program.
-Use constants when the value should remain constant throughout the program.
-
-Immutability Benefits: Constants provide safety by ensuring that once a value is set, it cannot be accidentally changed, reducing the risk of unintended modifications.
-Readability: */
-
-
+//Using String interpolation to print Pirkkala county info.
 let numberOfMunicipalities: Int = 22
 
 var population: Int
@@ -81,11 +51,9 @@ population = 529100
 let townName: String = "Pirkkala"
 
 var unemploymentRate: Double = 7.6
-
-
- //using String interpolation to calculate the actual number of unemployed people
  
 
+//Determining the no. of unemployed people in Pirkkala
 let unemployedPeople = Double(population) * (unemploymentRate / 100.00)
 
 let municipalDescription = "\(townName) has \(numberOfMunicipalities) municipalities, a population of \(population), and approximately \(unemployedPeople) unemployed people"
@@ -95,33 +63,29 @@ print(municipalDescription)
 
 
 
-/**
-
-Basic Data Types
-- Int = Integer
-- Double = Floating-Point values
-- Boolean = Boolean values
-- String = text
- 
- Collection Types - 3 Primary Types
- - Array
- - Dictionary
- - Set
- 
- Advanced Data Types
- - Tuples (helps to create and pass around grouping of values from a function)
- - Optionals (handles absence of values.*****/
-
-var welcomeMessage: String
-welcomeMessage = "Hello, there!"
-
-//multiple related types on a single line
-var red, green, blue: Double
-red = 101.00
-green = 404.00
-blue = 333.00
-print(red, green, blue)
+//String: Here five examples of declaring Swift String
 
 
+//Declaring a String
+let geeting : String = "Hello, Swift"
+
+
+//String Concatenation
+var firstName = "Charles"
+let lastName = "Nebo"
+let fullName = firstName + " " + lastName
+
+
+//String Interpolation
+let myAge: Int = 23
+let message = "I am \(myAge) years old. "
+
+//String length
+let courseName = "Swift Programming"
+let length = courseName.count
+
+//Extracting SubString
+let sentence = "Swift is powerful"
+let substring = sentence.prefix(5)
  
 
