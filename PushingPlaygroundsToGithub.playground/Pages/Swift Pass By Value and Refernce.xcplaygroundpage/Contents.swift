fecd32e4d6491d2cbd5567169440f067e59a4bd5
Charles Nebo <charles.nebo@hotmail.com>
2024-02-05 15:19:59 +0200

    added more contents in the pass by value folder

--- PushingPlaygroundsToGithub.playground/Pages/Swift Pass By Value and Refernce.xcplaygroundpage/Contents.swift
+++ PushingPlaygroundsToGithub.playground/Pages/Swift Pass By Value and Refernce.xcplaygroundpage/Contents.swift
@@ -26,12 +26,16 @@ newPoint.x = 5.0
 print(point.x) // output: 2.0 (original value remains unchanged)
 
 // Example 2:
-struct Person{
+struct iPerson{
     var name: String
     var age: Int
 }
 //Value Call: Creates the instance of a Person
-var person = Person(name: "John Doe", age: 25)
+var person = iPerson(name: "John Doe", age: 25)
+var myperson = person
+print(myperson.name)
+print(myperson.age)
+
 
 
 //Example 3:
@@ -144,4 +148,335 @@ enum Planet: CaseIterable {
 for planet in Planet.allCases {
     print(planet)
 }
+
+
+//Examples of Primitive Types:
+
+let originalNumber: Int = 42
+var newNumber = originalNumber //A copy of the value is made here.
+newNumber = 100        // Modifying the copy does not affect the original
+print(originalNumber)              // Output : 42 ( original value remains unchanged
+
+
+//Example 2
+let distance: Double = 10.5
+var currentDistance = distance
+currentDistance = 400.66
+print(distance)
+print(currentDistance)
+
+//Example 3
+let character: Character = "A"
+var anotherCharacter = character
+anotherCharacter = "B"
+print(character)
+print(anotherCharacter)
+
+
+//Example 4
+let flag: Bool = true
+var anotherFlag = flag
+anotherFlag = false
+print(flag)
+print(anotherFlag)
+
+//Example 5
+let text: String = "Hello, Swift"
+var anotherString = text
+anotherString = "Hello, Objective-c"
+print(text)
+print(anotherString)
+
+/**
+ Passed By Reference - Reference types those that define objects with properties and methods.
+ In Swift, classes are reference types. When you work with a reference type, the data is passed by reference. This means that, instead of creating a copy of the original object, the reference to the object is passed around. Any modifications made to the object through one reference will affect all other references to the same object
+ */
+
+//Example 1 - Classes
+class Person {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+}
+
+//Creating an instance of the class
+let personA = Person(name: "John", age: 20)
+var personB = personA                    // Reference to the same object
+personB.name = "Jane"    //Modifying through one refernce affects the original object
+print(personB.name)
+print(personB.age)      //Output: Jane (original object is modified)
+
+/**
+ 
+ However, it's important to note that when you pass an object to a function, the reference itself is passed by value. That is, the function receives a copy of the reference to the same object. If the function modifies the object, those changes will be visible outside the function because the reference is shared.
+ 
+ 
+ Statement 1:
+
+ "This demonstrates the concept of passing by reference for class instances in Swift, where changes made inside the function are reflected outside the function since classes are reference types."
+ Statement 2:
+
+ "However, it's important to note that when you pass an object to a function, the reference itself is passed by value. That is, the function receives a copy of the reference to the same object. If the function modifies the object, those changes will be visible outside the function because the reference is shared."
+
+ */
+
+//Example 2
+func modifiedPerson(_ person: Person){
+    person.name = "Jacob"
+    person.age = 50
+    
+}
+let person3 = Person(name: "Alice", age: 40)
+modifiedPerson(person3)
+print(person3.name)
+print(person3.age)
+
+
+//Example 3
+
+class Car {
+    var brand: String
+    var model: String
+    
+    init(brand: String, model: String) {
+        self.brand = brand
+        self.model = model
+    }
+}
+let car1 = Car(brand: "Mazda 3", model: "2018")
+var car2 = car1
+car2.brand = "CX 5"
+car2.model = "2022"
+print(car2.brand)
+print(car2.model)
+
+//Example 4
+class Animal {
+    var species: String
+    init(species: String) {
+        self.species = species
+    }
+}
+
+let mySpecies = Animal(species: "Tiger")
+var otherSpecies = mySpecies
+otherSpecies.species = "Chimpanzee"
+print(otherSpecies.species)
+
+//Example 5
+class iBook {
+    var title: String
+    var author: String
+    
+    init(title: String, author: String) {
+        self.title = title
+        self.author = author
+    }
+}
+
+let myBook = iBook(title: "One week one trouble", author: "Anezi, Okoro")
+var anotherBook = myBook
+anotherBook.title = "Not without my daughter"
+print(anotherBook.title)
+
+
+//Example 6
+class Team {
+    var name: String
+    var size: Int
+    
+    init(name: String, size: Int) {
+        self.name = name
+        self.size = size
+    }
+}
+
+let iTeam = Team(name: "ITproject Management team", size: 7)
+var specialTeam = iTeam
+specialTeam.name = "RE Team"
+specialTeam.size = 5
+
+print(specialTeam.name)
+print(specialTeam.size)
+
+
+//Functions: - Reference types that represent block of code.
+
+//Examples 1.
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+var sumResult = add(3, 4)
+print(sumResult)
+
+
+//Example 2
+func greet(name: String) -> String {
+    return "hello, \(name)"
+}
+var greeting = greet(name: "Charlie")
+print(greeting)
+
+
+//Example 3
+func square(_ x: Double) -> Double {
+    return x * x
+}
+
+let mySquare = square(4.0)
+let mySquare2 = square(5.0)
+print(mySquare)
+print(mySquare2)
+
+//Example 4
+func concatenate(_ str1: String, _ str2: String) -> String {
+    return str1 + "," + str2
+}
+let myConcatenate = concatenate("hello", "Charles")
+let myConcatenate2 = concatenate("hello", "Susana")
+print(myConcatenate)
+print(myConcatenate2)
+
+
+//Example 5
+
+func isEven(_ number: Int) -> Bool {
+    return number % 2 == 0
+}
+let myisEven = isEven(4)
+let myisEven2 = isEven(7)
+
+print(myisEven)
+print(myisEven2)
+
+//stopped here.
+//Other Instances with Examples
+
+// Tuple - Passed by Value
+
+struct PersonInfo {
+  var name: String
+  var age: Int
+}
+
+var personInfo = PersonInfo(name: "Alice", age: 35)
+var newPersonInfo = personInfo
+newPersonInfo.name = ("Bob")
+print(personInfo.name)
+
+
+
+
+//tuple
+func processTuple(input: (Int, String)) -> (Int,String) {
+    //Function logic here
+    return (input.0, "Processed: " + input.1)
+    
+}
+ let output = processTuple(input: (14, "Kolanuts"))
+  print(output)
+
+
+/**
+ Differences in Return and Print Statements.
+ return Statement:
+ Purpose: Used to send a value back from a function.
+ Functionality: Indicates the end of the function's execution and returns the specified value to the caller.
+ Usage: Typically used to produce a result or output that can be captured and used by the calling code.
+ 
+ print Statement:
+ Purpose: Used to output information to the console for debugging or informational purposes.
+ Functionality: Prints the specified values or expressions to the console during the program's execution.
+ Usage: Primarily used for debugging or displaying information but doesn't provide a value back to the caller.
+ 
+ */
+
+//Examples:
+func addSum(_ a: Int, _ b: Int) -> Int{
+    return a + b
+}
+let result = add(3, 5)
+print(result)
+
+//Example
+func calculateRectangleArea(length: Double, width: Double) -> Double{
+    return length * width
+}
+let areaOfRectangle = calculateRectangleArea(length: 5.0, width: 6.0)
+
+//Displaying the result
+print("Area of triangle: \(areaOfRectangle)")
+
+
+//Printing a Welcome message.
+func printWelcomeMessage() {
+    print("Welcome to Swift Programming!")
+}
+printWelcomeMessage()
+
+
+//Displaying the contents of an Array.
+func printArrayContents(_ array: [String]) {
+    for element in array {
+        print(element)
+    }
+}
+
+let myArray = ["Apple", "Banana", "Orange"]
+printArrayContents(myArray)
+
+// Function definition: Returns a concatenated string of array elements.
+func concatenatedArrayContents(_ array: [String]) -> String {
+    var result = " "
+    for element in array {
+        result += element + " "                   //result + element + " "
+    }
+    
+    return result
+}
+ //Function Call
+let mArray = ["Mango", "Okra", "Lime"]
+let concatenatedString = concatenatedArrayContents(mArray)
+
+//Print the returned  value
+print(concatenatedString)
+
+
+//Example: Logging information about a user
+func logUserInfo(name: String, age: Int) {
+    print("User Info: Name - \(name), Age - \(age)")
+}
+
+/**
+ 
+ Differences in Void functions and Return Types
+Void Funtion (No Return Types):
+ void function (no return type) performs some action but doesn't return a value explicitly.
+A function with a return type explicitly specifies the type of value it will return and uses the return keyword to send that value back to the caller
+ 
+ */
+
+// Void function (no return type)
+func greeti(name: String) {
+    print("Hello, \(name)!")
+}
+
+// Calling the void function
+greeti(name: "John")
+
+
+
+// Function with return type (Int)
+func addNumbers(a: Int, b: Int) -> Int {
+    return a + b
+}
+
+// Calling the function with return type
+let sum = addNumbers(a: 5, b: 3)
+print("Sum: \(sum)")
 
