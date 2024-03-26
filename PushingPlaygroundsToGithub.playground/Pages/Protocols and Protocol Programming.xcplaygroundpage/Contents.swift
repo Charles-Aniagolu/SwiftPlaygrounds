4cb0a89c7cbad63f297bc8338f3ab6ef012d7e64
Charles-Aniagolu <charles.nebo@hotmail.com>
2024-03-26 14:35:46 +0200

    Added more codes examples in the Protocol folder

--- PushingPlaygroundsToGithub.playground/Pages/Protocols and Protocol Programming.xcplaygroundpage/Contents.swift
+++ PushingPlaygroundsToGithub.playground/Pages/Protocols and Protocol Programming.xcplaygroundpage/Contents.swift
@@ -14,9 +14,11 @@ import UIKit
  Abstraction: Protocols allow you to define a contract without
  specifying the implementation details. This helps in abstracting
  away the specific implementation and focusing on what needs to be done.
+ 
  Encapsulation: Protocols encapsulate functionality by defining a set of
  requirements that types must adhere to. This promotes modular and reusable code.
- Polymorphism: Protocols enable polymorphism, allowing different types to be 
+ 
+ Polymorphism: Protocols enable polymorphism, allowing different types to be
  treated uniformly if they conform to the same protocol. This fosters flexibility
  and extensibility in your codebase.
  
@@ -24,15 +26,110 @@ import UIKit
  Defining and Adopting Protocols:
  - To define a protocol, use the protocol keyword followed by
    the protocol's name and its requirements.
- - Types can adopt protocols by conforming to them using the : syntax.
+ - Types can adopt protocols by conforming to them using them
+ - Swift Protocol asr concrete data types that can adopt a protocol by conforming to its requirements. When a type conforms to protocol, it means that the type implements all the methods, properties, and other requirements defined by the protocol: Examples of Types: struct, enum, Classes, Tuples
+ 
+ Concrete Types
+ 
+ - In Swift, a type is considered concrete if it can be instantiated directly, meaning you can create instances of that type. Concrete types have methods and properties associated with them, and can be customized as needed.In contrast, a protocol itself is not a concrete type because it cannot be directly instantiated. However, types that conform to a protocol can be concrete types and are capable of instantiation.
+ - Concrete types can include classes, structs, enums, tuples, and even some built-in types provided by Swift.
+ 
+ 
+    - Examples of concrete types in Swift include:
+        - Structs: For example, struct Person { var name: String }
+        - Classes: For example, class Car { var brand: String }
+        - Enums: For example, enum CompassDirection { case north, south, east, west }
+        
+ 
+ 
+ : syntax.
    Example:Protocol Drawable
      Here, Drawable protocol defines a method draw() and a property fillColor
  without specifying their implementation
 */
 
+// Structs - Example
+struct Person: CustomStringConvertible {
+   var name: String
+   var age: Int
+    
+    
+    var description: String { // here it's the description that implements the protocol
+        
+        return "Name:  \(name),  Age: \(age)"
+    }
+}
+let person = Person(name: "Donald", age: 73)
+print(person)
+/*
+ Explanation: The Person struct conforms to the CustomStringConvertible protocol by implementing the description property requirement. This allows instances of Person to provide a custom textual representation.
+ */
 
 
-// Defining and implementing protocols
+
+
+
+// Classes
+class Vehicle: Equatable {
+    var model: String
+    var year: Int
+    
+    init(model: String, year: Int) {
+        self.model = model
+        self.year = year
+    }
+    static func == (lhs: Vehicle, rhs: Vehicle) -> Bool { // Here it's the equality operator that implements the protocol
+        
+        return lhs.model == rhs.model && lhs.year == rhs.year
+    }
+}
+/*
+Explanation: The Vehicle class conforms to the Equatable protocol by implementing the == operator requirement. This allows instances of Vehicle to be compared for equality using the == operator.
+ */
+
+
+// Enum - Example.
+enum CompassDirection: CaseIterable {
+    case north
+    case south
+    case east
+    case west
+    
+    
+//CompassDirection conforms to Protocol by implementing allCases property required by CaseIterable
+    
+    static var allCases: [CompassDirection] {
+        return [.north, .south, .east, .west]
+    }
+}
+print("All compass directions:")
+for direction in CompassDirection.allCases {
+    print(direction)
+}
+/*
+ The CompassDirection enum conforms to the CaseIterable protocol by adding conformance to it after its declaration.
+ The allCases property is implemented as a static property of the CompassDirection enum, returning an array containing all the cases of the enum (north, south, east, and west).
+ We then use a for loop to iterate over all the cases returned by the allCases property and print each direction.
+ */
+
+typealias Numbers = (Int, Int)
+
+func < (lhs: Numbers, rhs: Numbers) -> Bool {
+    return lhs.0 < rhs.0 && lhs.1 < rhs.1
+}
+
+let numbers1: Numbers = (3, 5)
+let numbers2: Numbers = (2, 8)
+let numbers3: Numbers = (3, 6)
+
+print(numbers1 < numbers2) // Output: false
+print(numbers1 < numbers3) // Output: true
+
+
+
+
+
+
 
 //Example 1: Define a protocol for geometric shapes, where each shape has properties like area and perimeter.
 
@@ -54,6 +151,19 @@ struct Circle: Shape {
         return 2 * Double.pi * radius
     }
 }
+
+struct Square: Shape {
+    let side: Double
+    
+    var area: Double {
+        return side * side
+    }
+    var perimeter: Double {
+        return 4 * side
+    }
+}
+
+
 
 // Implementing the Rectangle struct conforming to the Shape protocol
 struct Rectangle: Shape {
@@ -84,3 +194,33 @@ print("\nRectangle:")
 print("Width: \(rectangle.width), Height: \(rectangle.height)")
 print("Area: \(rectangle.area)")
 print("Perimeter: \(rectangle.perimeter)")
+
+
+// Defining Protocols represnting basic bhaviours
+protocol Animal {
+    var name: String { get }
+    func makeSound()
+    
+}
+
+struct Dog: Animal {
+    let name: String
+    
+    func makeSound() {
+        print("Woof!")
+    }
+}
+
+struct Cat: Animal {
+    let name: String
+    
+    func makeSound() {
+        print("Meow!")
+    }
+}
+
+let dog = Dog(name: "Nappi")
+let cat = Cat(name: "Merkku")
+
+print(dog.name)
+print(cat.name)
